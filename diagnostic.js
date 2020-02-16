fbb384bc4898dd5d1fbb6f67b4ce70125209c832
Anthony Trinh <trinhan@flip2.engr.oregonstate.edu>
2020-02-15 22:26:21 -0800

    Server Side code for home and trains

--- diagnostic.js
+++ diagnostic.js
@@ -1,35 +1,89 @@
 var express = require('express');
 var mysql = require('./dbcon.js');
+var path = require('path');
+
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-
+//app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '/public')));
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 
-app.get('/',function(req,res,next){
-  var context = {};
-  var createString = "CREATE TABLE diagnostic(" +
-  "id INT PRIMARY KEY AUTO_INCREMENT," +
-  "text VARCHAR(255) NOT NULL)";
-  mysql.pool.query('DROP TABLE IF EXISTS diagnostic', function(err){
-    if(err){
-      next(err);
-      return;
+
+/*
+var bool = true;
+
+app.get(`/`, function (req, res, next) {
+  mysql.pool.query( function (err) {
+    if (err) {
+      return next(err)'';
     }
-    mysql.pool.query(createString, function(err){
-      if(err){
-        next(err);
-		return;
-      }
-	  mysql.pool.query('INSERT INTO diagnostic (`text`) VALUES ("MySQL is Working!")',function(err){
-	    mysql.pool.query('SELECT * FROM diagnostic', function(err, rows, fields){
-		  context.results = JSON.stringify(rows);
-		  res.render('home',context);
-		});
-	  });
-    });
+  });
+  var tables = [
+  "CREATE TABLE IF NOT EXISTS `trains` (" +
+  "`trainID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT," +
+  "`model` varchar(255) NOT NULL," +
+  "`cost` int(11) NOT NULL," +
+  "`capacity` int(11) NOT NULL," +
+  "`conductor` varchar(11) NOT NULL);",
+
+  "CREATE TABLE IF NOT EXISTS `stations` (" +
+  "`stationID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT," +
+  "`stationname` varchar(255) NOT NULL," +
+  "`address` varchar(255) NOT NULL," +
+  "`state` varchar(255) NOT NULL," +
+  "`city` varchar(255) NOT NULL," +
+  "`zipcode` int(11) NOT NULL,);",
+
+  "CREATE TABLE IF NOT EXISTS `routes` (" +
+  "`routeID` int(11) NOT NULL AUTO_INCREMENT," +
+  "`trainID` int(11), DEFAULT NULL" +
+  "FOREIGN KEY (trainID) REFERENCES trains(trainID)," +
+  "PRIMARY KEY (routeID));",
+
+  "CREATE TABLE IF NOT EXISTS `routesthrustations` (" +
+  "`routesthrustationsID` int(11) NOT NULL AUTO_INCREMENT," +
+  "`routeID` int(11)," +
+  "`stationID` int(11)," +
+  "`stoporder` int(11) NOT NULL," +
+  "`travelduration` varchar(255) not NULL," +
+  "FOREIGN KEY (routeID) REFERENCES routes(routeID)," +
+  "FOREIGN KEY (stationID) REFERENCES stations(stationID)," +
+  "PRIMARY KEY (routesthrustationsID));"
+  ];
+
+  var deleteTable = [
+  "DROP TABLE IF EXISTS `routesthrustations`",
+  "DROP TABLE IF EXISTS `routes`",
+  "DROP TABLE IF EXISTS `trains`",
+  "DROP TABLE IF EXISTS `stations`"
+  ];
+
+  var insert = [
+  "INSERT INTO `trains` (`model`, `cost`, `capacity`, `conductor`) VALUES (\"Steinway\", 100000, 250, \"John Doe\")",
+  "INSERT INTO `stations` (`stationname`, `address`, `city`, `state`, `zipcode`) VALUES (\"Maldurks\", \"0000 NE 60TH AVE\", \"Oregon\", \"Portland\", 97200)",
+  "INSERT INTO `routes` (`routeID`) VALUES (5)",
+  "INSERT INTO `routesthrustations` (`stoporder`, `travelduration`) VALUES (3, 50)"
+  ];
+
+});
+
+*/
+
+
+app.get('/home',function(req,res,next){
+  var context = {};
+  mysql.pool.query('SELECT * FROM trains', function(error, results, fields){
+    res.render('home');
+  });
+});
+
+app.get('/trains',function(req,response,next){
+  var context = {};
+  mysql.pool.query('SELECT * FROM trains', function(error, results, fields){
+    response.render('trains', {data: results});
   });
 });
 
@@ -38,11 +92,14 @@ app.use(function(req,res){
   res.render('404');
 });
 
+
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500);
   res.render('500');
 });
+
+
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
---
+++ public/index.js
@@ -0,0 +1,199 @@
+/* Button that pops up form to add element for each entity */
+function showmodal() {
+  var allboxes = document.getElementsByClassName("insert");
+  var i;
+  for (i = 0; i < allboxes.length; i++) {
+    allboxes[i].classList.toggle("show");
+  }
+}
+
+
+/* Inserts Train into flex container */
+// function inserttrain() {
+//   /* Close Form */
+//   document.getElementsByClassName("insert")[0].classList.toggle("show");
+//
+//   /* Making Train Insert Box */
+//   var traininsert = document.createElement("section");
+//   traininsert.classList.add("card");
+//
+//
+//   var train_number = document.createElement("div");
+//   var train_number_value = document.getElementById("train-number").value;
+//   var divText1 = document.createTextNode("Train " + train_number_value);
+//   train_number.classList.add("train-number");
+//   train_number.appendChild(divText1);
+//
+//
+//   var train_route = document.createElement("div");
+//   var train_route_value = document.getElementById("train-route").value;
+//   var divText2 = document.createTextNode("On Route: " + train_route_value);
+//   train_route.classList.add("train-route");
+//   train_route.appendChild(divText2);
+//
+//
+//   var train_model = document.createElement("div");
+//   var train_model_value = document.getElementById("train-model").value;
+//   var divText3 = document.createTextNode("Model: " + train_model_value);
+//   train_model.classList.add("train-model");
+//   train_model.appendChild(divText3);
+//
+//
+//   var train_price = document.createElement("div");
+//   var train_price_value = document.getElementById("train-price").value;
+//   var divText4 = document.createTextNode("Price: $" + train_price_value);
+//   train_price.classList.add("train-price");
+//   train_price.appendChild(divText4);
+//
+//   var train_capacity = document.createElement("div");
+//   var train_capacity_value = document.getElementById("train-capacity").value;
+//   var divText5 = document.createTextNode("Capacity: " + train_capacity_value);
+//   train_capacity.classList.add("train-capacity");
+//   train_capacity.appendChild(divText5);
+//
+//   var train_conductor = document.createElement("div");
+//   var train_conductor_value = document.getElementById("train-conductor").value;
+//   var divText6 = document.createTextNode("Conductor: " + train_conductor_value);
+//   train_conductor.classList.add("train-conductor");
+//   train_conductor.appendChild(divText6);
+//
+//   traininsert.appendChild(train_number);
+//   traininsert.appendChild(train_route);
+//   traininsert.appendChild(train_model);
+//   traininsert.appendChild(train_price);
+//   traininsert.appendChild(train_capacity);
+//   traininsert.appendChild(train_conductor);
+//
+//   /* Adding it into container */
+//   document.getElementById("card-container").appendChild(traininsert);
+//
+//   train_number_value = "";
+//   train_route_value = "";
+//   train_model_value = "";
+//   train_price_value = "";
+//   train_capacity_value = "";
+//   train_conductor_value = "";
+//
+//   document.getElementsByClassName("insert")[0].reset();
+//
+// }
+//
+// /* ------------------------------------------------------------------------- */
+//
+//
+// /* Sub-function for adding stations so we can easily create station boxes of any values
+//    rather than just from user input in forms */
+// function addstation(station_number_value, station_name_value, station_address_value,
+//                     station_state_value, station_city_value, station_zipcode_value) {
+//   /* Close Form */
+//   document.getElementsByClassName("insert")[0].classList.toggle("show");
+//
+//   /* Making Station Insert Box */
+//   var stationinsert = document.createElement("section");
+//   stationinsert.classList.add("card");
+//
+//   var station_number = document.createElement("div");
+//   var divText1 = document.createTextNode("Station #" + station_number_value);
+//   station_number.classList.add("station-number");
+//   station_number.appendChild(divText1);
+//
+//   var station_name = document.createElement("div");
+//   var divText2 = document.createTextNode("Name: " + station_name_value);
+//   station_name.classList.add("station-name");
+//   station_name.appendChild(divText2);
+//
+//   var station_address = document.createElement("div");
+//   var divText3 = document.createTextNode("Address: " + station_address_value);
+//   station_address.classList.add("station-address");
+//   station_address.appendChild(divText3);
+//
+//   var station_state = document.createElement("div");
+//   var divText4 = document.createTextNode("State: " + station_state_value);
+//   station_state.classList.add("station-state");
+//   station_state.appendChild(divText4);
+//
+//   var station_city = document.createElement("div");
+//   var divText5 = document.createTextNode("City: " + station_city_value);
+//   station_city.classList.add("station-city");
+//   station_city.appendChild(divText5);
+//
+//   var station_zipcode = document.createElement("div");
+//   var divText6 = document.createTextNode("Zipcode: " + station_zipcode_value);
+//   station_zipcode.classList.add("station-zipcode");
+//   station_zipcode.appendChild(divText6);
+//
+//   stationinsert.appendChild(station_number);
+//   stationinsert.appendChild(station_name);
+//   stationinsert.appendChild(station_address);
+//   stationinsert.appendChild(station_state);
+//   stationinsert.appendChild(station_city);
+//   stationinsert.appendChild(station_zipcode);
+//
+//   document.getElementById("card-container").appendChild(stationinsert);
+// }
+
+
+/* Adds stations but with only the variable names, not a template such as "Name: " + value */
+// function addstationonly(station_number_value, station_name_value, station_address_value,
+//                     station_state_value, station_city_value, station_zipcode_value) {
+//   /* Close Form */
+//   document.getElementsByClassName("insert")[0].classList.toggle("show");
+//
+//   /* Making Station Insert Box */
+//   var stationinsert = document.createElement("section");
+//   stationinsert.classList.add("card");
+//
+//   var station_number = document.createElement("div");
+//   var divText1 = document.createTextNode("Station #" + station_number_value);
+//   station_number.classList.add("station-number");
+//   station_number.appendChild(divText1);
+//
+//   var station_name = document.createElement("div");
+//   var divText2 = document.createTextNode("Name: " + station_name_value);
+//   station_name.classList.add("station-name");
+//   station_name.appendChild(divText2);
+//
+//   var station_address = document.createElement("div");
+//   var divText3 = document.createTextNode("Address: " + station_address_value);
+//   station_address.classList.add("station-address");
+//   station_address.appendChild(divText3);
+//
+//   var station_state = document.createElement("div");
+//   var divText4 = document.createTextNode("State: " + station_state_value);
+//   station_state.classList.add("station-state");
+//   station_state.appendChild(divText4);
+//
+//   var station_city = document.createElement("div");
+//   var divText5 = document.createTextNode("City: " + station_city_value);
+//   station_city.classList.add("station-city");
+//   station_city.appendChild(divText5);
+//
+//   var station_zipcode = document.createElement("div");
+//   var divText6 = document.createTextNode("Zipcode: " + station_zipcode_value);
+//   station_zipcode.classList.add("station-zipcode");
+//   station_zipcode.appendChild(divText6);
+//
+//   stationinsert.appendChild(station_number);
+//   stationinsert.appendChild(station_name);
+//   stationinsert.appendChild(station_address);
+//   stationinsert.appendChild(station_state);
+//   stationinsert.appendChild(station_city);
+//   stationinsert.appendChild(station_zipcode);
+//
+//   document.getElementById("card-container").appendChild(stationinsert);
+// }
+//
+//
+//
+// /* Inserts Station into flex container */
+// function insertstation() {
+//   var station_number_value = document.getElementById("station-number").value;
+//   var station_name_value = document.getElementById("station-name").value;
+//   var station_address_value = document.getElementById("station-address").value;
+//   var station_state_value = document.getElementById("station-state").value;
+//   var station_city_value = document.getElementById("station-city").value;
+//   var station_zipcode_value = document.getElementById("station-zipcode").value;
+//   addstation(station_number_value, station_name_value, station_address_value,
+//              station_state_value, station_city_value, station_zipcode_value);
+//   document.getElementsByClassName("insert")[0].reset();
+// }
