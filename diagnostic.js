1c58cf47dca39fbb39490bcbee4b3967b4a7c5ac
Anthony Trinh <trinhan@flip3.engr.oregonstate.edu>
2020-02-22 19:43:20 -0800

    Added Stations Route

--- diagnostic.js
+++ diagnostic.js
@@ -11,68 +11,6 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
 
-
-/*
-var bool = true;
-
-app.get(`/`, function (req, res, next) {
-  mysql.pool.query( function (err) {
-    if (err) {
-      return next(err)'';
-    }
-  });
-  var tables = [
-  "CREATE TABLE IF NOT EXISTS `trains` (" +
-  "`trainID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT," +
-  "`model` varchar(255) NOT NULL," +
-  "`cost` int(11) NOT NULL," +
-  "`capacity` int(11) NOT NULL," +
-  "`conductor` varchar(11) NOT NULL);",
-
-  "CREATE TABLE IF NOT EXISTS `stations` (" +
-  "`stationID` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT," +
-  "`stationname` varchar(255) NOT NULL," +
-  "`address` varchar(255) NOT NULL," +
-  "`state` varchar(255) NOT NULL," +
-  "`city` varchar(255) NOT NULL," +
-  "`zipcode` int(11) NOT NULL,);",
-
-  "CREATE TABLE IF NOT EXISTS `routes` (" +
-  "`routeID` int(11) NOT NULL AUTO_INCREMENT," +
-  "`trainID` int(11), DEFAULT NULL" +
-  "FOREIGN KEY (trainID) REFERENCES trains(trainID)," +
-  "PRIMARY KEY (routeID));",
-
-  "CREATE TABLE IF NOT EXISTS `routesthrustations` (" +
-  "`routesthrustationsID` int(11) NOT NULL AUTO_INCREMENT," +
-  "`routeID` int(11)," +
-  "`stationID` int(11)," +
-  "`stoporder` int(11) NOT NULL," +
-  "`travelduration` varchar(255) not NULL," +
-  "FOREIGN KEY (routeID) REFERENCES routes(routeID)," +
-  "FOREIGN KEY (stationID) REFERENCES stations(stationID)," +
-  "PRIMARY KEY (routesthrustationsID));"
-  ];
-
-  var deleteTable = [
-  "DROP TABLE IF EXISTS `routesthrustations`",
-  "DROP TABLE IF EXISTS `routes`",
-  "DROP TABLE IF EXISTS `trains`",
-  "DROP TABLE IF EXISTS `stations`"
-  ];
-
-  var insert = [
-  "INSERT INTO `trains` (`model`, `cost`, `capacity`, `conductor`) VALUES (\"Steinway\", 100000, 250, \"John Doe\")",
-  "INSERT INTO `stations` (`stationname`, `address`, `city`, `state`, `zipcode`) VALUES (\"Maldurks\", \"0000 NE 60TH AVE\", \"Oregon\", \"Portland\", 97200)",
-  "INSERT INTO `routes` (`routeID`) VALUES (5)",
-  "INSERT INTO `routesthrustations` (`stoporder`, `travelduration`) VALUES (3, 50)"
-  ];
-
-});
-
-*/
-
-
 app.get('/home',function(req,res,next){
   var context = {};
   mysql.pool.query('SELECT * FROM trains', function(error, results, fields){
@@ -80,10 +18,16 @@ app.get('/home',function(req,res,next){
   });
 });
 
-app.get('/trains',function(req,response,next){
+app.get('/stations', function(req, res, next) {
+  mysql.pool.query('SELECT * FROM stations', function(error, results, fields){
+    res.render('stations', {data: results});
+  });
+});
+
+app.get('/trains',function(req, res, next){
   var context = {};
   mysql.pool.query('SELECT * FROM trains', function(error, results, fields){
-    response.render('trains', {data: results});
+    res.render('trains', {data: results});
   });
 });
 
