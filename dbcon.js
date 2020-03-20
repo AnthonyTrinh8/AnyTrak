61f59fce5beb041850a719de01bef4c541566d28
Anthony Trinh <trinhan@flip1.engr.oregonstate.edu>
2020-03-19 20:23:57 -0700

    Removed Name

--- dbcon.js
+++ dbcon.js
@@ -2,9 +2,9 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_trinhan',
-  password        : '7107',
-  database        : 'cs340_trinhan'
+  user            : '',
+  password        : '',
+  database        : ''
 });
 
 module.exports.pool = pool;
